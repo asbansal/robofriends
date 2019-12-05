ebe67f5ba36ef428775d6fa4060c508bd06f35d6
Ankur Bansal <48391146+asbansal@users.noreply.github.com>
2019-12-05 17:38:55 +0530

    Updates

---
+++ precache-manifest.0973977c0daea81dc9ae08b5b7fe8d0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a99f042033beb84317cbb44864769cb",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "d2974db0a539dbc58b81",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "586a879a293f4186c3f6",
+    "url": "/robofriends/static/css/main.72aa4faa.chunk.css"
+  },
+  {
+    "revision": "d2974db0a539dbc58b81",
+    "url": "/robofriends/static/js/2.0a7b8027.chunk.js"
+  },
+  {
+    "revision": "586a879a293f4186c3f6",
+    "url": "/robofriends/static/js/main.008e20cb.chunk.js"
+  },
+  {
+    "revision": "e02f0353cb7f83d53758",
+    "url": "/robofriends/static/js/runtime-main.952b26ca.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
